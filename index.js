f101ad5de485b4985bf62647b7621b4405cfbc4a
JoshBenPhilip <joshben4c@gmail.com>
2022-04-29 14:03:25 -0400

    created working restaurant maker and deletion code

--- index.js
+++ index.js
@@ -1,8 +1,79 @@
+// const admin = require("firebase-admin");
 
-const admin = require("firebase-admin");
+// const serviceAccount = require('./credentials.json');
 
-const serviceAccount = require("path/to/serviceAccountKey.json");
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+// const db = admin.firestore();
+
+import admin from "firebase-admin";// imports firebase library of tools
+
+import serviceAccount from './credentials.js'; // import our credentials to connect to firebase
+
+admin.initializeApp({//connects to our firebase project
+  credential: admin.credential.cert(serviceAccount) // creating a certificate from our credentials
 });
+
+//Now we are connected to OUR Firebase project & related services
+
+const db = admin.firestore(); // creates a shortcut to access the Firestore database
+
+const restaurantsCol = db.collection('restaurants') // shortcut to point to our collection
+
+
+
+const restaurant = {// creating an object -- in this case a specific restaurant 
+                    // we want to add to our database
+
+    name: 'Mister 01',
+    address: '555 N Federal Hwy Suite #5, Boca Raton, Fl 33432',
+    cuisine: 'Pizza',
+    rating: '4.9',
+    phone: '(786) 677-2903',
+}////below is the way to add stuff to mongo or firebase
+// db.collection('restaurants').add(restaurant)
+// .then(doc => console.log('Created restaurant', doc.id))
+// .catch(err => console.error(err))
+////this is todd style automatically add the try catch
+
+////below is the way to add stuff to mongo or firebase
+// db.collection('restaurants')
+//.add(restaurant)
+// .then(doc => console.log('Created restaurant', doc.id))
+// .catch(err => console.error(err))
+////this is todd style automatically add the try catch
+
+
+//////below is the way to add stuff to mongo or firebase
+// db.collection('restaurants').add(restaurant)
+// .then(doc => console.log('Created restaurant', doc.id))
+// .catch(err => console.error(err))
+////this is todd style automatically add the try catch
+
+restaurantsCol.add(restaurant) // adding the restaurant to our restaurants collection
+.then(doc => console.log ('Created restaurant',doc.id))
+.catch(err => console.error('err'))
+
+
+
+
+
+
+const restaurant2 = {
+    name: 'Bolay',
+    address: '7060 W Palmeto Park rd, Boca Raton, FL 33433',
+    cuisine:'American',
+    rating: 4.6,
+}
+
+async function addRestaurant(data) {
+    try{
+    const doc = await db.collection('restaurants').add(restaurant2)
+    console.log('Created restaurant', doc.id)
+    } catch(err){
+        console.error('err')
+    }
+}
+addRestaurant(restaurant2)
