0f16b65888084ce11f489710697ac946a655a23f
JoshBenPhilip <joshben4c@gmail.com>
2022-04-29 15:03:48 -0400

    made code dryer

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// const admin = require("firebase-admin");
-
-// const serviceAccount = require('./credentials.json');
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
-// const db = admin.firestore();
-
-import admin from "firebase-admin";// imports firebase library of tools
-
-import serviceAccount from './credentials.js'; // import our credentials to connect to firebase
-
-admin.initializeApp({//connects to our firebase project
-  credential: admin.credential.cert(serviceAccount) // creating a certificate from our credentials
-});
-
-//Now we are connected to OUR Firebase project & related services
-
-const db = admin.firestore(); // creates a shortcut to access the Firestore database
-
-const restaurantsCol = db.collection('restaurants') // shortcut to point to our collection
-
-
 
 const restaurant = {// creating an object -- in this case a specific restaurant 
                     // we want to add to our database
